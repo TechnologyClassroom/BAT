46a3c918de747b725e76c970edcd52f0f0e5ae78
BlueHillBGCB <mmcmahon@bgcb.org>
2016-05-17 15:34:26 -0400

    update_updatehosts.bat

--- updatehosts.bat
+++ updatehosts.bat
@@ -1,3 +1,14 @@
+:: Michael McMahon
+:: This script downloads HOSTS files from github.com/BlueHillBGCB/HOSTS on Windows systems.
+
+:: MUST BE RUN WITH ADMIN CMD
+
 @ECHO OFF
+:: Create a directory on disk.
+if not exist C:\hosts mkdir C:\hosts
 powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFwin.txt'; $path = 'c:\hosts\HOSTSFwin.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
 powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRwin.txt'; $path = 'c:\hosts\HOSTSMTWRwin.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
+:: Formatting from ::http://blog.gpunktschmitz.com/504-powershell-download-file-from-server-via-https-which-has-a-self-signed-certificate
+
+:: Overwrite HOSTS file with MTWR file
+copy C:\hosts\HOSTSMTWRwin.txt C:\WINDOWS\System32\drivers\etc\hosts

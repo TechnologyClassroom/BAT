b534f6cfb3e43b965cb9070f927e81f946c553cf
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-30 15:50:50 -0400

    Update updatehosts.bat

--- updatehosts.bat
+++ updatehosts.bat
@@ -1,14 +1,14 @@
-:: updatehosts.bat
+:: undatehosts.bat
 :: Michael McMahon
 :: This script downloads HOSTS files from github.com/BlueHillBGCB/HOSTS on Windows systems.
-
+ 
 :: MUST BE RUN WITH ADMIN CMD
-
+ 
 @ECHO OFF
 
-:: Copy a file and append the date
+:: Copy a file and append the date.  Then, delete the history.
 set backupFilename=_%date:~-4,4%%date:~-7,2%%date:~-10,2%
-copy "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\History" "C:\hosts\history%backupFilename%"
+copy "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\History" "C:\hosts\history%backupFilename%" && del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\History"
 :: Formatting from https://community.tableau.com/thread/172730
 :: Formatting from Bernhard Hofmann on http://stackoverflow.com/questions/864718/how-to-append-a-date-in-batch-files
 
@@ -37,21 +37,17 @@ del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\Tr
 del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\Visited Links"
 del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\Web Data"
 del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\Web Data-journal"
-del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\History"
 del /s /f /q "C:\Users\bluehill\AppData\Local\Google\Chrome\User Data\Default\History Provider Cache"
 
 :: Create a directory on disk.
 if not exist C:\hosts mkdir C:\hosts
-powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFwin.txt'; $path = 'c:\hosts\HOSTSFwin.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
-powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRwin.txt'; $path = 'c:\hosts\HOSTSMTWRwin.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
-powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFwinLS.txt'; $path = 'c:\hosts\HOSTSFwinLS.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
-powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRwinLS.txt'; $path = 'c:\hosts\HOSTSMTWRwinLS.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 1>NUL
-:: Formatting from ::http://blog.gpunktschmitz.com/504-powershell-download-file-from-server-via-https-which-has-a-self-signed-certificate
-
-:: This copies the file to hosts depending on the day of the week
+powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFwinLS.txt'; $path = 'c:\hosts\HOSTSFwinLS.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 
+powershell "$url = 'https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRwinLS.txt'; $path = 'c:\hosts\HOSTSMTWRwinLS.txt'; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = new-object System.Net.WebClient; $webClient.DownloadFile( $url, $path )" 
+:: Formatting from http://blog.gpunktschmitz.com/504-powershell-download-file-from-server-via-https-which-has-a-self-signed-certificate
+ 
+:: This copies the hosts file depending on the day
 setlocal 
 for /f "delims=" %%a in ('wmic path win32_localtime get dayofweek /format:list ') do for /f "delims=" %%d in ("%%a") do set %%d 
-IF %dayofweek%==5 (copy /Y C:\hosts\HOSTSFwin.txt C:\windows\system32\drivers\etc\hosts) ELSE (copy /Y C:\hosts\HOSTSMTWRwin.txt C:\windows\system32\drivers\etc\hosts) 
-::IF %dayofweek%==5 (copy /Y C:\hosts\HOSTSFwinLS.txt C:\windows\system32\drivers\etc\hosts) ELSE (copy /Y C:\hosts\HOSTSMTWRwinLS.txt C:\windows\system32\drivers\etc\hosts) 
+IF %dayofweek%==5 (copy /Y C:\hosts\HOSTSFwinLS.txt C:\windows\system32\drivers\etc\hosts) ELSE (copy /Y C:\hosts\HOSTSMTWRwinLS.txt C:\windows\system32\drivers\etc\hosts) 
 endlocal 
 :: Formatting from http://stackoverflow.com/questions/11364147/setting-a-windows-batch-file-variable-to-the-day-of-the-week 
